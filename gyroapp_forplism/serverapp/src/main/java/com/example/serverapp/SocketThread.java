b105b572569e1d366cab7ca0e82ee19d49310dba
fx32948 <hiroyuki.tojo@fujixerox.co.jp>
2023-01-16 20:18:42 +0900

    Modified the serverapp to be able to display receieved angle data.

--- gyroapp_forplism/serverapp/src/main/java/com/example/serverapp/SocketThread.java
+++ gyroapp_forplism/serverapp/src/main/java/com/example/serverapp/SocketThread.java
@@ -1,4 +1,5 @@
 package com.example.serverapp;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,9 +8,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
 
 public class SocketThread extends Thread{
     private AngleDataMessageQueue angleDataMessageQueue;
+    private AngleData angleData;
     private boolean running = true;
     private MessageQueueListenerInterface messageQueuelistener = null;
     public SocketThread(){
@@ -25,16 +28,23 @@ public class SocketThread extends Thread{
                 //BufferedReader reader = new BufferedReader(inputStream);
                 try (OutputStream sender = socket.getOutputStream(); InputStream receiver = socket.getInputStream()) {
                     while (true) {
-                        byte[] data = new byte[4];
-                        receiver.read(data, 0, 4);
+                        byte[] lengthData = new byte[4];
+                        receiver.read(lengthData, 0, 4);
                         // ByteBufferを通ってlittleエンディアンで変換してデータサイズを受け取る。
-                        ByteBuffer bb = ByteBuffer.wrap(data);
+                        ByteBuffer bb = ByteBuffer.wrap(lengthData);
                         bb.order(ByteOrder.LITTLE_ENDIAN);
                         int length = bb.getInt();
                         // データサイズほど、バッファーを設定する。
-                        data = new byte[length];
+                        byte[] data = new byte[length];
                         // データを受け取る。
                         receiver.read(data, 0, length);
+
+                        int direction = data[0];
+                        int rollX = data[1] + data[2] * 0x100;
+                        int pitchY = data[3] + data[4] * 0x100;
+                        int azimuthZ = data[5] + data[6] * 0x100;
+                        AngleData angleData = new AngleData(direction, rollX, pitchY, azimuthZ);
+                        angleDataMessageQueue.add(angleData);
                     }
                 }
             }
