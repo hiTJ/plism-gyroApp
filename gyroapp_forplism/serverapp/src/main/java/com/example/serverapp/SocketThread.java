e838b569a7376764e16ee507dd4d84790a716af6
hiTJ <tojohiroyuki0511@yahoo.co.jp>
2023-02-15 19:03:55 +0900

    Modified the serverapp to be able to disconnect and re-connect.

--- gyroapp_forplism/serverapp/src/main/java/com/example/serverapp/SocketThread.java
+++ gyroapp_forplism/serverapp/src/main/java/com/example/serverapp/SocketThread.java
@@ -31,44 +31,49 @@ public class SocketThread extends Thread{
             InetSocketAddress ipep = new InetSocketAddress(10000);
             // ソケット接続
             server.bind(ipep);
-            try (Socket socket = server.accept()) {
-                isConnected = true;
-                try (OutputStream sender = socket.getOutputStream(); InputStream receiver = socket.getInputStream()) {
-                    while (true) {
-                        isRunning = true;
-                        byte[] lengthData = new byte[4];
-                        receiver.read(lengthData, 0, 4);
-                        // ByteBufferを通ってlittleエンディアンで変換してデータサイズを受け取る。
-                        ByteBuffer bb = ByteBuffer.wrap(lengthData);
-                        bb.order(ByteOrder.LITTLE_ENDIAN);
-                        int length = bb.getInt();
-                        // データサイズほど、バッファーを設定する。
-                        byte[] data = new byte[length];
-                        // データを受け取る。
-                        receiver.read(data, 0, length);
+            while(true) {
+                isConnected = false;
+                try (Socket socket = server.accept()) {
+                    isConnected = true;
+                    try (OutputStream sender = socket.getOutputStream(); InputStream receiver = socket.getInputStream()) {
+                        while (true) {
+                            isRunning = true;
+                            byte[] lengthData = new byte[4];
+                            receiver.read(lengthData, 0, 4);
+                            // ByteBufferを通ってlittleエンディアンで変換してデータサイズを受け取る。
+                            ByteBuffer bb = ByteBuffer.wrap(lengthData);
+                            bb.order(ByteOrder.LITTLE_ENDIAN);
+                            int length = bb.getInt();
+                            // データサイズほど、バッファーを設定する。
+                            byte[] data = new byte[length];
+                            // データを受け取る。
+                            receiver.read(data, 0, length);
 
-                        // ByteBufferを通ってデータサイズをbyteタイプに変換する。
-                        ByteBuffer b = ByteBuffer.allocate(4);
-                        // byteフォマートはlittleエンディアンだ。
-                        b.order(ByteOrder.LITTLE_ENDIAN);
-                        b.putInt(data.length);
-                        // データ長さを送信
-                        sender.write(b.array(), 0, 4);
-                        // データ送信
-                        sender.write(data);
+                            // ByteBufferを通ってデータサイズをbyteタイプに変換する。
+                            ByteBuffer b = ByteBuffer.allocate(4);
+                            // byteフォマートはlittleエンディアンだ。
+                            b.order(ByteOrder.LITTLE_ENDIAN);
+                            b.putInt(data.length);
+                            // データ長さを送信
+                            sender.write(b.array(), 0, 4);
+                            // データ送信
+                            sender.write(data);
 
-                        int direction = data[0];
-                        int rollX = data[1] * 0x100 + (data[2] & 0xFF);
-                        int pitchY = data[3] * 0x100 + (data[4] & 0xFF);
-                        int azimuthZ = data[5] * 0x100 + (data[6] & 0xFF);
-                        int initialization = data[7];
-                        AngleData angleData = new AngleData(direction, rollX, pitchY, azimuthZ, initialization);
-                        if(initialization == 1){
-                            setInitAngleData(angleData);
+                            int direction = data[0];
+                            int rollX = data[1] * 0x100 + (data[2] & 0xFF);
+                            int pitchY = data[3] * 0x100 + (data[4] & 0xFF);
+                            int azimuthZ = data[5] * 0x100 + (data[6] & 0xFF);
+                            int initialization = data[7];
+                            AngleData angleData = new AngleData(direction, rollX, pitchY, azimuthZ, initialization);
+                            if (initialization == 1) {
+                                setInitAngleData(angleData);
+                            }
+                            setCurrentAngleData(angleData);
+                            angleDataMessageQueue.add(angleData);
                         }
-                        setCurrentAngleData(angleData);
-                        angleDataMessageQueue.add(angleData);
                     }
+                }catch(Exception ex) {
+                    ex.printStackTrace();
                 }
             }
         }catch (IOException e){
