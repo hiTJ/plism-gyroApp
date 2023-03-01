a344c28eea9136e9c690bd76057d3d14a3b55001
fx32948 <hiroyuki.tojo@fujixerox.co.jp>
2023-03-01 10:58:14 +0900

    Modified BluetoothThreadClass to connect and send angle data to ESP32.

--- gyroapp_forplism/serverapp/src/main/java/com/example/serverapp/BluetoothThread.java
+++ gyroapp_forplism/serverapp/src/main/java/com/example/serverapp/BluetoothThread.java
@@ -33,15 +33,34 @@ public class BluetoothThread extends Thread{
         this.bluetoothAdapter = bluetoothAdapter;
         this.bluetoothSerial = new BluetoothSerial(this.bluetoothAdapter);
     }
+    private AngleData pollAngleData(){
+        AngleData result = null;
+        int init = 0;
+        for(int i = 0; i < angleDataMessageQueue.size(); i++){
+            result = angleDataMessageQueue.poll();
+            if(result.initialization == 1){
+                init = 1;
+            }
+        }
+        if(result != null){
+            result.initialization = init;
+        }
+
+        return result;
+    }
 
     private void connect2Stand(){
-        this.bluetoothSerial.connectToDevice("MAC ADDRESS");
-        while(!this.bluetoothSerial.isConnected()){
-            try{
-                Thread.sleep(100);
-            }catch (InterruptedException e){
-                e.printStackTrace();
+        try {
+            this.bluetoothSerial.connectToDevice("94:E6:86:12:14:D2");
+            while (!this.bluetoothSerial.isConnected()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
         this.isConnected = true;
     }
@@ -52,35 +71,48 @@ public class BluetoothThread extends Thread{
                 isRunning = false;
                 connect2Stand();
             }else{
-                AngleData angleData = this.angleDataMessageQueue.poll();
-                if(angleData == null || initializedAngleData == null){
+                AngleData angleData = pollAngleData();
+                if(angleData == null){
                     continue;
                 }
                 if(angleData.initialization == 1){
                     this.initializedAngleData = angleData;
                     Log.d("debug", "Do initialization!!");
                 }
+                if(initializedAngleData == null){
+                    continue;
+                }
                 //下方向が正にで通知されているので、反転させる
                 int currentX = this.initializedAngleData.pitchX - angleData.pitchX;
                 //右方向が正にで通知されているので、init時の値をそのままマイナスする
                 int currentZ =  angleData.azimuthZ - this.initializedAngleData.azimuthZ;
-                if(currentX > 45){
-                    currentX = 45;
+                currentX = currentX + 60;
+                if(currentX > 119){
+                    currentX = 119;
                 }
                 if(currentX < 0){
                     currentX = 0;
                 }
+                currentZ = currentZ + 180;
                 currentZ = currentZ < 0 ? currentZ + 360 : currentZ;
                 int initialization = angleData.initialization;
                 byte[] dataX = ByteBuffer.allocate(4).putInt(currentX).array();
                 byte[] dataZ = ByteBuffer.allocate(4).putInt(currentZ).array();
                 byte[] x = castBytesToWORD(dataX);
                 byte[] z = castBytesToWORD(dataZ);
-                byte[] serial = concat(x, z);
-                this.bluetoothSerial.write(serial);
+                String strx = "y" + String.valueOf(currentX) + " ";
+                String strz = "x" + String.valueOf(currentZ) + "\n";
+                byte[] sbytex = strx.getBytes();
+                byte[] sbytez = strz.getBytes();
+                this.bluetoothSerial.write(concat(sbytex, sbytez));
                 Log.d("debug", "X: " + String.valueOf(x));
                 Log.d("debug", "Z: " + String.valueOf(z));
                 Log.d("debug", "Init: " + String.valueOf(initialization));
+            }
+            try {
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
